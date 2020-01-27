e41a69be07fe282cc649892ff15ff51041b00ee0
Christopher Pham <c1pham@student.bridgew.edu>
2020-01-27 13:37:04 -0500

    Neccesary edits

--- GatherJobs.py
+++ GatherJobs.py
@@ -1,47 +1,43 @@
-import json
 import requests
 import time
 # https://www.geeksforgeeks.org/get-post-requests-using-python/
-# this is page I used for reference to learn http
-# https://realpython.com/python-json/
-# this is for learning JSON
+# this is page I used for reference to learn request
 
 
 def main():
-    jobs = []
+    jobs = []  # hold jobs
     jobs = getJobs(jobs)
-    print(len(jobs))
     fileName = "jobs.txt"
     writeJobsToFile(jobs, fileName)
 
-    readFromFile()
-
 
 def getJobs(jobsList):
+    # Link to API that retreives job posting data
     gitJobsURL = "https://jobs.github.com/positions.json?"
     pageNum = 0
+    # retreives about 5 pages, puts all jobs in the job list
     while pageNum < 5:
         pageNum = pageNum + 1
-        parameters = {'page': pageNum}
-        req = requests.get(url=gitJobsURL, params=parameters)
-        data = req.json()
-        transferObjToList(data, jobsList)
-        time.sleep(1)
+        parameters = {'page': pageNum}  # param to get jobs from a specific page
+        req = requests.get(url=gitJobsURL, params=parameters)  # get jobs
+        jobsFromAPI = req.json()
+        transferObjToList(jobsFromAPI, jobsList)
+        time.sleep(.1)
     return jobsList
 
 
 def writeJobsToFile(jobsList, fileName):
     writing_file = open(fileName, 'w')
-    for page in jobsList:
-        #print(page)
-        print(page, file=writing_file)
+    for entry in jobsList:
+        print(entry, file=writing_file)
     writing_file.close()
 
 
+# move list from API to python job list
 def transferObjToList(jsonList, receiveList):
     for entry in jsonList:
         receiveList.append(entry)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # if running from this file, then run the main function
     main()
--- test/testGatherJobs.py
+++ test/testGatherJobs.py
@@ -5,13 +5,18 @@ def test_Getting_jobs():
     jobs = []
     jobs = GatherJobs.getJobs(jobs)
     numJobs = len(jobs)
-
-   assert numJobs > 99
+    # checks to see if it gathered the right amount of jobs
+    assert numJobs > 100
 
 
 def testWriteFile():
-    main()
     jobFile = open("jobs.txt", 'r')
-
     linesInFile = jobFile.readlines()
+    count = 0
+    # this looks through the document to find an line that has a job title that I know should be there
+    for line in linesInFile:
+        indicator = line.find("(Senior-) Full Stack TypeScript Developer (m/w/d)")
+        if indicator != -1:
+            count = count + 1
+    assert count != 0
 
