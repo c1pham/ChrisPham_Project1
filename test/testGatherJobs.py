4516ab292e186557c117e5cdc31d2efbdd9ac68a
Christopher Pham <c1pham@student.bridgew.edu>
2020-01-31 09:44:25 -0500

    remove format error

--- test/testGatherJobs.py
+++ test/testGatherJobs.py
@@ -13,15 +13,9 @@ def test_write_file():
     GatherJobs.main()
     job_file = open("jobs.txt", 'r')
     lines_in_file = job_file.readlines()
-    count = 0
     # this looks through the first line of document to find this job title because dump put all data in first line
     # find will return -1 if the string is not in there
     indicator = lines_in_file[0].find("(Senior-) Full Stack TypeScript Developer (m/w/d)")
     # if the find function returns anything else than -1, meaning it return position of string then the test passed
     assert indicator != 1
 
-    # for line in lines_in_file:
-    #    indicator = line.find("(Senior-) Full Stack TypeScript Developer (m/w/d)")
-    #    if indicator != -1:
-    #        count = count + 1
-    # assert count != 0
