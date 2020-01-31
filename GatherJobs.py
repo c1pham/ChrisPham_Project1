361729a9f64c28181614a8534e3ab30631d9c032
Christopher Pham <c1pham@student.bridgew.edu>
2020-01-31 09:36:29 -0500

    add json library to requirements.txt

--- GatherJobs.py
+++ GatherJobs.py
@@ -1,11 +1,12 @@
 import requests
 import time
 import json
+
+
 # https://www.geeksforgeeks.org/get-post-requests-using-python/
 # this is page I used for reference to learn request
 # https://developer.rhino3d.com/guides/rhinopython/python-xml-json/
 # this page help me to learn json
-
 
 
 def main():  # collect jobs from github jobs API and store into text file
@@ -24,26 +25,24 @@ def get_jobs(jobs_list):
         page_num = page_num + 1
         parameters = {'page': page_num}  # param to get jobs from a specific page
         req = requests.get(url=git_jobs_url, params=parameters)  # get jobs
-        if str(req) != "<Response [503]>":
+        if str(req) != "<Response [503]>":  # if message is not 503, then convert to json and print
             jobs_from_api = req.json()
             transfer_obj_to_list(jobs_from_api, jobs_list)
-            time.sleep(.2)
+            time.sleep(.1)
     return jobs_list
 
 
 def write_jobs_to_file(jobs_list, file_name):  # write dictionary objects into file
     writing_file = open(file_name, 'w')
-    for entry in jobs_list:
-        json.dump(entry, writing_file)
-        # dumping
-        # print(entry, file=writing_file)
+    json.dump(jobs_list, writing_file)
     writing_file.close()
 
 
-# move list from API to python job list
+# move job from API list to programs own job list
 def transfer_obj_to_list(json_list, receive_list):
-    for entry in json_list:
-        receive_list.append(entry)
+    # for entry in json_list:
+    #     receive_list.append(entry)
+    receive_list.extend(json_list)
 
 
 if __name__ == '__main__':  # if running from this file, then run the main function
--- test/testGatherJobs.py
+++ test/testGatherJobs.py
@@ -3,7 +3,7 @@ import GatherJobs
 
 def test_getting_jobs():
     jobs = []
-    jobs = GatherJobs.get_jobs(jobs)
+    jobs = GatherJobs.get_jobs(jobs)  # get jobs from API
     num_jobs = len(jobs)
     # checks to see if it gathered the right amount of jobs
     assert num_jobs > 100
@@ -14,9 +14,14 @@ def test_write_file():
     job_file = open("jobs.txt", 'r')
     lines_in_file = job_file.readlines()
     count = 0
-    # this looks through the document to find an line that has a job title that I know should be there
-    for line in lines_in_file:
-        indicator = line.find("(Senior-) Full Stack TypeScript Developer (m/w/d)")
-        if indicator != -1:
-            count = count + 1
-    assert count != 0
+    # this looks through the first line of document to find this job title because dump put all data in first line
+    # find will return -1 if the string is not in there
+    indicator = lines_in_file[0].find("(Senior-) Full Stack TypeScript Developer (m/w/d)")
+    # if the find function returns anything else than -1, meaning it return position of string then the test passed
+    assert indicator != 1
+
+    # for line in lines_in_file:
+    #    indicator = line.find("(Senior-) Full Stack TypeScript Developer (m/w/d)")
+    #    if indicator != -1:
+    #        count = count + 1
+    # assert count != 0
