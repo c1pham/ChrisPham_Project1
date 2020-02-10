e1fc82b7e42e61c310c4a69a5590cb2b99caaebf
Christopher Pham <c1pham@student.bridgew.edu>
2020-02-10 08:47:05 -0500

    add pytest fixture so won't have to repeatly reach out to API for jobs throughout test. Broke saving into database test into two different test, one for a specific job another for a specific number of jobs

--- test/testGatherJobs.py
+++ test/testGatherJobs.py
@@ -4,9 +4,17 @@ import os.path
 # this is website where I learned SQL command to check if it exist
 
 
-def test_getting_jobs():
+@pytest.fixture
+def get_data():
+    import GatherJobs  # this runs independently of everything else before code is run so we import here
     jobs = []
     jobs = GatherJobs.get_jobs(jobs)  # get jobs from API
+    return jobs
+
+
+def test_getting_jobs(get_data):
+    jobs = []
+    jobs = get_data  # get jobs from API
     num_jobs = len(jobs)
     # checks to see if it gathered the right amount of jobs
     assert num_jobs > 100
@@ -24,11 +32,10 @@ def test_getting_jobs():
 
 
 # checks to see if appropriate number of jobs save and if a specific job I know is there saved
-def test_save_to_db():
+def test_save_all_jobs_to_db(get_data):
     db_connection, db_cursor = GatherJobs.open_db("jobs_db")  # open db
     GatherJobs.create_jobs_table(db_cursor)
-    jobs = []  # hold jobs
-    jobs = GatherJobs.get_jobs(jobs)  # get jobs from Github jobs API
+    jobs = get_data  # hold jobs
     num_jobs_from_api = len(jobs)
     # num of jobs before programed save new jobs to db
     num_db_jobs_before_save = len(list(db_cursor.execute("SELECT * FROM JOBS;")))
@@ -39,13 +46,23 @@ def test_save_to_db():
     # checks how many jobs in db after new jobs are saved
     num_db_jobs_after_save = len(list(db_cursor.execute("SELECT * FROM JOBS;")))
 
+    GatherJobs.close_db(db_connection)  # close db connection
+    # if the number of new jobs added is same as the difference between jobs now and jobs before in db then test passed
+    assert num_jobs_from_api == num_db_jobs_after_save - num_db_jobs_before_save
+
+
+def test_save_specific_job_to_db(get_data):
+    db_connection, db_cursor = GatherJobs.open_db("jobs_db")  # open db
+    GatherJobs.create_jobs_table(db_cursor)
+    jobs = get_data  # hold jobs from Github jobs API
+    GatherJobs.save_git_jobs_to_db(db_cursor, jobs)  # add new jobs to db
+    GatherJobs.close_db(db_connection)
+
+    db_connection, db_cursor = GatherJobs.open_db("jobs_db")
     job_name = jobs[1]['title']  # this is title of 1st job from github jobs API
     result = db_cursor.execute("SELECT * FROM JOBS WHERE title = ?;", (job_name,))
     num_results = len(list(result))
     GatherJobs.close_db(db_connection)  # close db connection
-    # if the number of new jobs added is same as the difference between jobs now and jobs before in db then test passed
-    assert num_jobs_from_api == num_db_jobs_after_save - num_db_jobs_before_save
-    # if the results from this query is 1 or more, we know this job saved correctly to the db
     assert num_results >= 1
 
 
@@ -59,7 +76,6 @@ def test_create_table():
     # and the only item in the tuple is a count of how many tables have that name
     num_matches = list_result[0][0]
     assert num_matches == 1  # if that count is 1 then table is successfully created
-    # ALSO ASK PROFESSOR IF NEED TO COUNT FOR MORE TABLES
 
 
 # checks to see if database exist
@@ -67,5 +83,4 @@ def test_making_db():
     db_connection, db_cursor = GatherJobs.open_db("jobs_db")  # this will make database if it does not exist
     GatherJobs.close_db(db_connection)
     status = os.path.exists("jobs_db")  # returns true if this file exist
-    # ALSO ASK PROFESSOR IF WE CAN STOP HERE DO I NEED TO CHECK IF THIS FILE IS DB OR JUST IF IT EXIST
     assert status is True  # pass test if it is true this file exists
