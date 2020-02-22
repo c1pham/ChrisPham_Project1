c49085b4efd48333383092cbc72e44b9b0422668
Christopher Pham <c1pham@student.bridgew.edu>
2020-02-22 01:47:04 -0500

    made changes to mapview to make it customize to my preferences. Code structure is not changed. also remove a blank line from main.py to avoid a flake8 error

--- Main.py
+++ Main.py
@@ -29,4 +29,3 @@ def main():
 
 if __name__ == '__main__':  # if running from this file, then run the main function
     main()
-
--- MapView.py
+++ MapView.py
@@ -1,15 +1,16 @@
-import pandas as pd
+import pandas
 import ssl
 import plotly.express as px
 
+# https://plot.ly/python/mapbox-layers/
+# plotly map reference, use some code from their website
 
-def make_jobs_map(data: pd.DataFrame):
+
+def make_jobs_map(data: pandas.DataFrame):
     ssl._create_default_https_context = ssl._create_unverified_context
-    us_cities = data
-
-    print(us_cities)
-    print(type(us_cities))
-    fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="title", hover_data=["additional_info"],
+    job_data = data
+    fig = px.scatter_mapbox(job_data, lat="lat", lon="lon", hover_name="title",
+                            hover_data=["additional_info"],
                             color_discrete_sequence=["fuchsia"], zoom=2, height=300)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
