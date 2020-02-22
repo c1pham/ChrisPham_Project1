9082652a3f08f934b31ccdf76eb46d41b3ea63c3
Christopher Pham <c1pham@student.bridgew.edu>
2020-02-22 01:41:43 -0500

    added functions to take data and turn into dataframe. can make plotly open up an map in browser. Create cache to save location data with coordinates. Create function to make location table. Create function to add locations to location table. Made functions to check if a job or location is unique to its respective table. Edit date parsing function. Added some error checking to function that finds lat long coordinates. Create function to load into location cache. Create function to take job data from database tuple then make it into a dictionary.

---
+++ MapView.py
@@ -0,0 +1,18 @@
+import plotly.graph_objects as go
+import pandas as pd
+import ssl
+import plotly.express as px
+
+
+def make_jobs_map(data: pd.DataFrame):
+    ssl._create_default_https_context = ssl._create_unverified_context
+    us_cities = data
+
+    print(us_cities)
+    print(type(us_cities))
+    fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="title", hover_data=["additional_info"],
+                            color_discrete_sequence=["fuchsia"], zoom=2, height=300)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.show()
+
